fcfa547e7efc95fd67cdd8d328ace096f10b93c2
OfflineBot <flintstonelg@gmail.com>
2024-11-24 11:36:59 +0100

    ndarray trait

--- src/rsnet/ndarray_trait/flatten_to_twod.rs
+++ src/rsnet/ndarray_trait/flatten_to_twod.rs
@@ -1,5 +1,5 @@
 
-use ndarray::{Array2, Array4};
+use ndarray::{Array2, Array4, Axis};
 
 pub trait FlattenTo2D {
     fn flatten_to_2d(&self) -> Array2<f32>;
@@ -7,7 +7,14 @@ pub trait FlattenTo2D {
 
 impl FlattenTo2D for Array4<f32> {
     fn flatten_to_2d(&self) -> Array2<f32> {
-        println!("hello. you should implement this now :)");
-        Array2::zeros((1, 1))
+        let samples = self.shape()[0];
+        let size = self.shape()[1] * self.shape()[2] * self.shape()[3];
+
+        let mut output = Array2::zeros((samples, size));
+        for i in 0..samples {
+            let flattened = self.index_axis(Axis(0), i).flatten().iter().cloned().collect::<Vec<f32>>();
+            output.row_mut(i).assign(&Array2::from_shape_vec((1, size), flattened).unwrap().row(0));
+        }
+        output
     }
 }
